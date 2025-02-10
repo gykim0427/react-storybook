c531c5daf0f4596273087fbac660cdcece04c221
gykim <gykim0427@naver.com>
2025-02-10 15:23:08 +0900

    20250210 redbutton component add

---
+++ src/stories/RedButton.tsx
@@ -0,0 +1,35 @@
+import './redbutton.css';
+
+export interface NewButtonProps {
+  /** Is this the principal call to action on the page? */
+  primary?: boolean;
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: 'small' | 'medium' | 'large';
+  /** Button contents */
+  label: string;
+  /** Optional click handler */
+  onClick?: () => void;
+}
+
+/** Primary UI component for user interaction */
+export const RedButton = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: NewButtonProps) => {
+  const mode = primary ? 'storybook-redbutton--primary' : 'storybook-redbutton--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-redbutton', `storybook-redbutton--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
